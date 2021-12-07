e42551515464e04fc91286c75409e3d089c732be
Samuel Robertson <sprobertson94@gmail.com>
2021-12-06 21:34:58 -0700

    Logo update and favicon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { Link } from "remix";
+import Logo from "./Logo";
 export default function Navbar(){
   return (
     <header>
+      <div>
+        <Logo />
+      </div>
       <nav>
         <Link to="/">home</Link>
         <Link to="/posts">posts</Link>
